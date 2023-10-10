34962ec52c9b6a7e29b6e42f0a1a18053a9504ba
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-10-10 14:53:42 -0700

    added pkg docs

---
+++ clusterer/docs.go
@@ -0,0 +1,8 @@
+package clusterer
+
+// Package clusterer provides API for running Kmeans clustering algorithm.
+
+// The kmeans algorithms implemented are
+// - Lloyd's algorithm
+// - Lloyd's with Kmeans++ initializer
+// - Elkan's algorithm
---
+++ initializer/docs.go
@@ -0,0 +1,7 @@
+package initializer
+
+// Package initializer provides API for initializing centroids.
+
+// Initializing algorithms implemented are
+// - Random initialization
+// - Kmeans++ initialization
