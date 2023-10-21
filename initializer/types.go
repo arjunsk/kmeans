24f53ef5315718940a60df4c69cf7b8b67efb5c6
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-10-20 22:15:17 -0700

    Added Tests

--- initializer/types.go
+++ initializer/types.go
@@ -15,11 +15,8 @@ func validateArgs(vectors [][]float64, clusterCnt int) error {
 	if clusterCnt <= 0 {
 		return errors.New("KMeans: k cannot be less than or equal to zero")
 	}
-	if len(vectors) == 0 {
+	if vectors == nil || len(vectors) == 0 {
 		return errors.New("KMeans: data cannot be empty")
-	}
-	if vectors == nil || len(vectors[0]) == 0 {
-		return errors.New("KMeans: data cannot be nil")
 	}
 	return nil
 }
