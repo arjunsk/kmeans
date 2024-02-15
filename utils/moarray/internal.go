634066a4bbc31701b3d80fec6feaef5a686b1f48
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-02-15 13:59:52 -0800

    minor

--- utils/moarray/internal.go
+++ utils/moarray/internal.go
@@ -37,6 +37,6 @@ func NormalizeGonumVectors(vectors []*mat.VecDense) {
 // NormalizeMoVecf64 is used only in test functions.
 func NormalizeMoVecf64(vector []float64) []float64 {
 	res := ToGonumVector[float64](vector)
-	//NormalizeGonumVector(res)
+	NormalizeGonumVector(res)
 	return ToMoArray[float64](res)
 }
